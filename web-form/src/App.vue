78cf88b2a6e69829e9e7c5fce352c77319aaf1d1
xjaylandero <xjaylandero@gmail.com>
2023-12-05 16:08:05 +0800

    Started Sign Up

---
+++ web-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <SignUpFormVue />
+</template>
+
+<script>
+import SignUpFormVue from "./components/SignUpForm.vue";
+
+export default {
+  name: "App",
+  components: { SignUpFormVue },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  background-color: #141414;
+  margin: 0;
+}
+</style>
